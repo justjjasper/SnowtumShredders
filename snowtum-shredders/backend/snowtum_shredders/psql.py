94ead3743964de19d544062923a6c21075e4b7e6
JASPER <jasjasper101@gmail.com>
2023-10-02 10:06:30 -0700

    Implement snowboards/shirts data schemas

--- snowtum-shredders/backend/snowtum_shredders/psql.py
+++ snowtum-shredders/backend/snowtum_shredders/psql.py
@@ -34,14 +34,56 @@ CREATE TABLE IF NOT EXISTS snowboards (
   sidecut VARCHAR(255) NOT NULL,
   flex VARCHAR(255) NOT NULL,
   rider_type VARCHAR(255) NOT NULL,
-  tech_story VARCHAR(255) NOT NULL,
+  tech_story TEXT NOT NULL,
   camber_type VARCHAR(255) NOT NULL,
-  camber_description VARCHAR(255) NOT NULL,
+  camber_description TEXT NOT NULL,
   camber_image VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS snowboard_images (
-
+  snowboard_image_id PRIMARY KEY NOT NULL,
+  snowboard_id INTEGER NOT NULL,
+  snowboard_image VARCHAR(255) NOT NULL,
+  FOREIGN KEY (snowboard_id) REFERENCES snowboards(snowboard_id),
+  INDEX (snowboard_id)
 );
+
+CREATE TABLE IF NOT EXISTS snowboard_reviews (
+  review_id SERIAL PRIMARY KEY NOT NULL,
+  snowboard_id INTEGER NOT NULL,
+  snowboard_review_title VARCHAR(255) NOT NULL,
+  snowboard_review_author VARCHAR(255) NOT NULL,
+  snowboard_review_date DATE NOT NULL,
+  snowboard_review_body TEXT NOT NULL,
+  FOREIGN KEY (snowboard_id) REFERENCES snowboards(snowboard_id),
+  INDEX (snowboard_id)
+);
+
+CREATE TABLE IF NOT EXISTS snowboard_skus (
+  snowboard_sku_id INTEGER PRIMARY KEY NOT NULL,
+  snowboard_id INTEGER NOT NULL,
+  snowboard_size NUMERIC NOT NULL,
+  snowboard_sku NUMERIC NOT NULL,
+  FOREIGN KEY (snowboard_id) REFERENCES snowboards(snowboard_id),
+  INDEX (snowboard_id)
+);
+
+CREATE TABLE IF NOT EXISTS tshirts (
+  tshirt_id INTEGER PRIMARY KEY NOT NULL,
+  tshirt_name VARCHAR(255) NOT NULL,
+  tshirt_price NUMERIC NOT NULL,
+  tshirt_image VARCHAR(255) NOT NULL,
+  tshirt_description TEXT
+);
+
+CREATE TABLE IF NOT EXISTS tshirt_skus (
+  tshirt_sku_id INTEGER PRIMARY KEY NOT NULL,
+  tshirt_id INTEGER NOT NULL,
+  tshirt_size VARCHAR(255) NOT NULL,
+  tshirt_sku NUMERIC NOT NULL,
+  FOREIGN KEY (tshirt_id) REFERENCES tshirts(tshirt_id),
+  INDEX (tshirt_id)
+);
+
 
 '''
