065e7e6cd8c7e7e864be772bec0602126893de3b
JASPER <jasjasper101@gmail.com>
2023-10-25 17:50:55 -0700

    Complete snowboard collections mobile layout

--- snowtum-shredders/app/components/Collections/Snowboards/SnowboardList.tsx
+++ snowtum-shredders/app/components/Collections/Snowboards/SnowboardList.tsx
@@ -11,13 +11,13 @@ type SnowboardListProps = {
 export default function SnowboardList({ products }: SnowboardListProps) {
   // sm:gap-20 xl:gap-24
   return (
-    <div className='content-list flex flex-row flex-wrap md:gap-20 xl:gap-24 justify-start px-5 lg:px-24'>
+    <div className='content-list flex flex-row flex-wrap gap-y-12 lg:gap-20 xl:gap-24 justify-start px-5 lg:px-24'>
       {/* Maps out each Snowboard Product*/}
       {products.map((snowboard: SnowboardProductType, i: number) => {
         const formattedSnowboardName = snowboard.snowboard_name.toLowerCase().replace(/\s+/g, '-');
 
         return (
-          <div className='product-container flex flex-col sm:border-2 justify-start font-semibold w-6/12 lg:w-3/12'
+          <div className='product-container flex flex-col justify-start font-semibold w-6/12 lg:w-3/12'
             key={i}
           >
             <Link className='product-image flex mb-5' href={`/products/snowboard/${formattedSnowboardName}`}>
@@ -40,7 +40,7 @@ export default function SnowboardList({ products }: SnowboardListProps) {
                     <Link
                       href={isInactive ? '#' : `/products/snowboard/${formattedSnowboardName}`}
                       key={i}
-                      className={`relative flex justify-center border-[1px] font-normal text-xs py-[1px] rounded-xl w-1/5 ${isInactive ? 'opacity-50 text-gray-400 pointer-events-none disabled-size' : ''}`}
+                      className={`relative flex justify-center border-[1px] font-normal text-[9px] md:text-xs py-[1px] rounded-xl w-1/5 ${isInactive ? 'opacity-50 text-gray-400 pointer-events-none disabled-size' : ''}`}
                     >
                       {metaData.size}
                     </Link>
@@ -52,7 +52,7 @@ export default function SnowboardList({ products }: SnowboardListProps) {
                     <Link
                       href={`/products/snowboard/${formattedSnowboardName}`}
                       key={i}
-                      className='relative flex justify-center font-normal text-xs py-[1px] rounded-xl w-1/5'
+                      className='relative flex justify-center font-normal text-[9px] md:text-xs py-[1px] rounded-xl w-1/7 xsm:w-1/5'
                     >
                       {`+${remainder} more`}
                     </Link>
@@ -61,13 +61,13 @@ export default function SnowboardList({ products }: SnowboardListProps) {
                 return null
               })}
             </div>
-            <span className='product-description flex mt-3 text-xs w-[35ch]'>
+            <span className='product-description flex mt-3 text-[11px] xsm:text-xs w-[23ch] md:w-[35ch]'>
               {snowboard.header_description.replace(/\s*\/\s*/g, ' ')}
             </span>
-            <span className='product-name flex text-2xl w-[20ch]'>
+            <span className='product-name flex py-[1.5px] xsm:py-0 text-[15px] md:text-2xl w-[20ch]'>
               <Link href={`/products/snowboard/${formattedSnowboardName}`}>{snowboard.snowboard_name}</Link>
             </span>
-            <span className='product-price flex font-normal'>${snowboard.snowboard_price}</span>
+            <span className='product-price flex font-normal text-[11px] xsm:text-sm md:text-base'>${snowboard.snowboard_price}</span>
           </div>
         )
       })}
