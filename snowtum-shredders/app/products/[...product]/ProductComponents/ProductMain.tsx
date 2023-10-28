942dc29de75d35eff4f097f6a4cb710fa4a52796
JASPER <jasjasper101@gmail.com>
2023-10-28 15:31:48 -0700

    implement magnifying plus svg, checkpoint before incorporating content-container grid

--- snowtum-shredders/app/products/[...product]/ProductComponents/ProductMain.tsx
+++ snowtum-shredders/app/products/[...product]/ProductComponents/ProductMain.tsx
@@ -1,7 +1,11 @@
 'use client'
 
+import { magnifyingPlusSVG } from "@/app/Misc/Icons"
+
 export default function ProductMain() {
   return (
-    <div className='product-main'></div>
+    <div className='product-main'>
+      {magnifyingPlusSVG}
+    </div>
   )
 }
