b9ceed76cee2b3eeba3a42f7916596dfe5a4e841
JASPER <jasjasper101@gmail.com>
2023-10-28 14:18:40 -0700

    Adjust navbar backdrop filter density

--- snowtum-shredders/app/components/Header/NavBar.tsx
+++ snowtum-shredders/app/components/Header/NavBar.tsx
@@ -110,7 +110,7 @@ export default function NavBar(){
   return (
     // **To Implement individual backdrop blur of Snowboard/Accessory dropdown menu** When state of either snowboard/acessories is hovered via onMouseEnter, a class is added to entire header to create backdrop blur.
     // Implement my-[-280/560px] to combat relative white space when snowboard/accessoriesMobileMenu is set to true
-    <div className={`header flex flex-col font-calibre sticky font-bold top-0 backdrop-blur-[1000px] z-50 text-[#ffffff] w-full
+    <div className={`header flex flex-col font-calibre sticky font-bold top-0 backdrop-blur-[35px] z-50 text-[#ffffff] w-full
       ${snowboardHovered ? 'snowboardMenuTrigger' : ''}
       ${accessoriesHovered ? 'accessoriesMenuTrigger' : ''}
       ${searchHovered ? 'searchMenuTrigger' : ''}
