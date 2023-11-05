e82e061de0ed5bd77f082c4fcf544b4ea0ec2e9c
JASPER <jasjasper101@gmail.com>
2023-11-05 12:05:00 -0800

    Fix accessory product details

--- snowtum-shredders/app/products/[...product]/ProductComponents/ProductAccessoryDetail.tsx
+++ snowtum-shredders/app/products/[...product]/ProductComponents/ProductAccessoryDetail.tsx
@@ -2,23 +2,26 @@ import { ProductType } from "@/app/types"
 import React from "react";
 
 export default function ProductAccessoryDetail ( {product}: {product: ProductType}) {
- // Function to add line breaks while preserving the bullets
-  function formatDescription(description:string) {
-    const items = description.split('•').map(item => item.trim()).filter(Boolean);
-    return items.map((item, index) => (
-      <React.Fragment key={index}>
-        {index > 0 && <br />} • {item}
-      </React.Fragment>
+  // Converts product description to format
+  function convertStringToJSX(inputString:string) {
+    const lines = inputString.split('\\n');
+    console.log(lines)
+    const jsxLines = lines.map((line, index) => (
+      <span key={index}>
+        {`• ${line}`}
+    </span>
     ));
+
+    return jsxLines;
   }
 
   return (
     <section className='content-container-product-accessory-detail max-width-screen-[1920px]'>
       <div className='content-wider lg:px-16'>
         <div className='product-detail-accessory border-[1px] rounded-[30px] p-[30px] my-[100px]'>
-          <div className='product-detail-accessory-content mx-6'>
+          <div className='product-detail-accessory-content flex flex-col mx-6'>
             <span className='product-detail-title uppercase'>product details</span>
-            <p>{formatDescription(product.description)}</p>
+            {convertStringToJSX(product.description)}
           </div>
         </div>
       </div>
