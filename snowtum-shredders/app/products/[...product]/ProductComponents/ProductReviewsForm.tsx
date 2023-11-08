0127e777f4f5a2ec876a78f35cd8751450f13017
JASPER <jasjasper101@gmail.com>
2023-11-07 21:26:55 -0800

    Adjust star rating css logic to have gray fill even after a rating is selected

--- snowtum-shredders/app/products/[...product]/ProductComponents/ProductReviewsForm.tsx
+++ snowtum-shredders/app/products/[...product]/ProductComponents/ProductReviewsForm.tsx
@@ -33,6 +33,12 @@ export default function ProductReviewsForm ( {product_id}: ProductReviewsFormPro
     return rating !==0 && rating >= currRating ? true : false
   }
 
+  const selectRating = (e: React.MouseEvent<HTMLAnchorElement>, starRating: number) => {
+    e.preventDefault()
+    setRating(starRating)
+    console.log(rating)
+  }
+
   return (
     // ! Be wary that there are solo value attr with each input in the form
     <div className='spr-form ' id={`form_${product_id}`}>
@@ -60,18 +66,14 @@ export default function ProductReviewsForm ( {product_id}: ProductReviewsFormPro
             {/* //! LOOK BACK AT THIS UGH Star Rating logic*/}
             <div className='spr-form-input spr-starrating flex gap-1'>
               {/* //TODO each a tag contains an onClick func */}
-              {/* //? each a contains classname of spr-icon-star-underline */}
               <a href='#'
                  className={`spr-icon spr-icon-star ${star1 ? 'spr-icon-star-underline' : ''} ${checkRating(1) ? 'spr-icon-star-not-empty' : ''}`}
                  data-value={'1'}
                  aria-label='1 of 5 stars'
                  onMouseEnter={() => setHovered(prevState => ({...prevState, star1:true}))}
                  onMouseLeave={() => setHovered(prevState => ({...prevState, star1:false}))}
-                 onClick={(e) => {
-                  e.preventDefault()
-                  setRating(1)
-                 }}>
-                 <svg viewBox="0 0 15 15" stroke={checkRating(1) ? 'gold' : 'gray'} aria-hidden={true} xmlns="http://www.w3.org/2000/svg" className='w-[17px] h-[17px] md:w-[25px] md:h-[25px]'> <path d="M7.22303 0.665992C7.32551 0.419604 7.67454 0.419604 7.77702 0.665992L9.41343 4.60039C9.45663 4.70426 9.55432 4.77523 9.66645 4.78422L13.914 5.12475C14.18 5.14607 14.2878 5.47802 14.0852 5.65162L10.849 8.42374C10.7636 8.49692 10.7263 8.61176 10.7524 8.72118L11.7411 12.866C11.803 13.1256 11.5206 13.3308 11.2929 13.1917L7.6564 10.9705C7.5604 10.9119 7.43965 10.9119 7.34365 10.9705L3.70718 13.1917C3.47945 13.3308 3.19708 13.1256 3.25899 12.866L4.24769 8.72118C4.2738 8.61176 4.23648 8.49692 4.15105 8.42374L0.914889 5.65162C0.712228 5.47802 0.820086 5.14607 1.08608 5.12475L5.3336 4.78422C5.44573 4.77523 5.54342 4.70426 5.58662 4.60039L7.22303 0.665992Z" fill={`${rating !== 0 ? checkRating(1) ? 'gold' : 'none' : star1 || star2 || star3 || star4 || star5 ? 'gray' : 'none'}`} /></svg>
+                 onClick={(e) => selectRating(e,1)}>
+                 <svg viewBox="0 0 15 15" stroke={checkRating(1) ? 'gold' : 'gray'} aria-hidden={true} xmlns="http://www.w3.org/2000/svg" className='w-[17px] h-[17px] md:w-[25px] md:h-[25px]'> <path d="M7.22303 0.665992C7.32551 0.419604 7.67454 0.419604 7.77702 0.665992L9.41343 4.60039C9.45663 4.70426 9.55432 4.77523 9.66645 4.78422L13.914 5.12475C14.18 5.14607 14.2878 5.47802 14.0852 5.65162L10.849 8.42374C10.7636 8.49692 10.7263 8.61176 10.7524 8.72118L11.7411 12.866C11.803 13.1256 11.5206 13.3308 11.2929 13.1917L7.6564 10.9705C7.5604 10.9119 7.43965 10.9119 7.34365 10.9705L3.70718 13.1917C3.47945 13.3308 3.19708 13.1256 3.25899 12.866L4.24769 8.72118C4.2738 8.61176 4.23648 8.49692 4.15105 8.42374L0.914889 5.65162C0.712228 5.47802 0.820086 5.14607 1.08608 5.12475L5.3336 4.78422C5.44573 4.77523 5.54342 4.70426 5.58662 4.60039L7.22303 0.665992Z" fill={`${rating !== 0 ? checkRating(1) ? 'gold' : star1 || star2 || star3 || star4 || star5 ? 'gray' : 'none' : star1 || star2 || star3 || star4 || star5 ? 'gray' : 'none'}`} /></svg>
               </a>
               <a href='#'
                 className={`spr-icon spr-icon-star ${star2 ? 'spr-icon-star-underline' : ''} ${checkRating(2) ? 'spr-icon-star-not-empty' : ''}`}
@@ -79,11 +81,8 @@ export default function ProductReviewsForm ( {product_id}: ProductReviewsFormPro
                 aria-label='2 of 5 stars'
                 onMouseEnter={() => setHovered(prevState => ({...prevState, star2:true}))}
                 onMouseLeave={() => setHovered(prevState => ({...prevState, star2:false}))}
-                onClick={(e) => {
-                  e.preventDefault()
-                  setRating(2)
-                 }}>
-                <svg viewBox="0 0 15 15" stroke={checkRating(2) ? 'gold' : 'gray'} aria-hidden={true} xmlns="http://www.w3.org/2000/svg" className='w-[17px] h-[17px] md:w-[25px] md:h-[25px]'> <path d="M7.22303 0.665992C7.32551 0.419604 7.67454 0.419604 7.77702 0.665992L9.41343 4.60039C9.45663 4.70426 9.55432 4.77523 9.66645 4.78422L13.914 5.12475C14.18 5.14607 14.2878 5.47802 14.0852 5.65162L10.849 8.42374C10.7636 8.49692 10.7263 8.61176 10.7524 8.72118L11.7411 12.866C11.803 13.1256 11.5206 13.3308 11.2929 13.1917L7.6564 10.9705C7.5604 10.9119 7.43965 10.9119 7.34365 10.9705L3.70718 13.1917C3.47945 13.3308 3.19708 13.1256 3.25899 12.866L4.24769 8.72118C4.2738 8.61176 4.23648 8.49692 4.15105 8.42374L0.914889 5.65162C0.712228 5.47802 0.820086 5.14607 1.08608 5.12475L5.3336 4.78422C5.44573 4.77523 5.54342 4.70426 5.58662 4.60039L7.22303 0.665992Z" fill={`${rating !== 0 ? checkRating(2) ? 'gold' : 'none' : star2 || star3 || star4 || star5 ? 'gray' : 'none'}`} /></svg>
+                onClick={(e) => selectRating(e,2)}>
+                <svg viewBox="0 0 15 15" stroke={checkRating(2) ? 'gold' : 'gray'} aria-hidden={true} xmlns="http://www.w3.org/2000/svg" className='w-[17px] h-[17px] md:w-[25px] md:h-[25px]'> <path d="M7.22303 0.665992C7.32551 0.419604 7.67454 0.419604 7.77702 0.665992L9.41343 4.60039C9.45663 4.70426 9.55432 4.77523 9.66645 4.78422L13.914 5.12475C14.18 5.14607 14.2878 5.47802 14.0852 5.65162L10.849 8.42374C10.7636 8.49692 10.7263 8.61176 10.7524 8.72118L11.7411 12.866C11.803 13.1256 11.5206 13.3308 11.2929 13.1917L7.6564 10.9705C7.5604 10.9119 7.43965 10.9119 7.34365 10.9705L3.70718 13.1917C3.47945 13.3308 3.19708 13.1256 3.25899 12.866L4.24769 8.72118C4.2738 8.61176 4.23648 8.49692 4.15105 8.42374L0.914889 5.65162C0.712228 5.47802 0.820086 5.14607 1.08608 5.12475L5.3336 4.78422C5.44573 4.77523 5.54342 4.70426 5.58662 4.60039L7.22303 0.665992Z" fill={`${rating !== 0 ? checkRating(2) ? 'gold' : star2 || star3 || star4 || star5 ? 'gray' : 'none' : star2 || star3 || star4 || star5 ? 'gray' : 'none'}`} /></svg>
               </a>
               <a href='#'
                  className={`spr-icon spr-icon-star ${star3 ? 'spr-icon-star-underline' : ''} ${checkRating(3) ? 'spr-icon-star-not-empty' : ''}`}
@@ -91,11 +90,8 @@ export default function ProductReviewsForm ( {product_id}: ProductReviewsFormPro
                  aria-label='3 of 5 stars'
                  onMouseEnter={() => setHovered(prevState => ({...prevState, star3:true}))}
                  onMouseLeave={() => setHovered(prevState => ({...prevState, star3:false}))}
-                 onClick={(e) => {
-                  e.preventDefault()
-                  setRating(3)
-                 }}>
-                <svg viewBox="0 0 15 15" stroke={checkRating(3) ? 'gold' : 'gray'} aria-hidden={true} xmlns="http://www.w3.org/2000/svg" className='w-[17px] h-[17px] md:w-[25px] md:h-[25px]'> <path d="M7.22303 0.665992C7.32551 0.419604 7.67454 0.419604 7.77702 0.665992L9.41343 4.60039C9.45663 4.70426 9.55432 4.77523 9.66645 4.78422L13.914 5.12475C14.18 5.14607 14.2878 5.47802 14.0852 5.65162L10.849 8.42374C10.7636 8.49692 10.7263 8.61176 10.7524 8.72118L11.7411 12.866C11.803 13.1256 11.5206 13.3308 11.2929 13.1917L7.6564 10.9705C7.5604 10.9119 7.43965 10.9119 7.34365 10.9705L3.70718 13.1917C3.47945 13.3308 3.19708 13.1256 3.25899 12.866L4.24769 8.72118C4.2738 8.61176 4.23648 8.49692 4.15105 8.42374L0.914889 5.65162C0.712228 5.47802 0.820086 5.14607 1.08608 5.12475L5.3336 4.78422C5.44573 4.77523 5.54342 4.70426 5.58662 4.60039L7.22303 0.665992Z" fill={`${rating !== 0 ? checkRating(3) ? 'gold' : 'none' : star3 || star4 || star5 ? 'gray' : 'none'}`} /></svg>
+                 onClick={(e) => selectRating(e,3)}>
+                <svg viewBox="0 0 15 15" stroke={checkRating(3) ? 'gold' : 'gray'} aria-hidden={true} xmlns="http://www.w3.org/2000/svg" className='w-[17px] h-[17px] md:w-[25px] md:h-[25px]'> <path d="M7.22303 0.665992C7.32551 0.419604 7.67454 0.419604 7.77702 0.665992L9.41343 4.60039C9.45663 4.70426 9.55432 4.77523 9.66645 4.78422L13.914 5.12475C14.18 5.14607 14.2878 5.47802 14.0852 5.65162L10.849 8.42374C10.7636 8.49692 10.7263 8.61176 10.7524 8.72118L11.7411 12.866C11.803 13.1256 11.5206 13.3308 11.2929 13.1917L7.6564 10.9705C7.5604 10.9119 7.43965 10.9119 7.34365 10.9705L3.70718 13.1917C3.47945 13.3308 3.19708 13.1256 3.25899 12.866L4.24769 8.72118C4.2738 8.61176 4.23648 8.49692 4.15105 8.42374L0.914889 5.65162C0.712228 5.47802 0.820086 5.14607 1.08608 5.12475L5.3336 4.78422C5.44573 4.77523 5.54342 4.70426 5.58662 4.60039L7.22303 0.665992Z" fill={`${rating !== 0 ? checkRating(3) ? 'gold' : star3 || star4 || star5 ? 'gray' : 'none' : star3 || star4 || star5 ? 'gray' : 'none'}`} /></svg>
               </a>
               <a href='#'
                  className={`spr-icon spr-icon-star ${star4 ? 'spr-icon-star-underline' : ''} ${checkRating(4) ? 'spr-icon-star-not-empty' : ''}`}
@@ -103,11 +99,8 @@ export default function ProductReviewsForm ( {product_id}: ProductReviewsFormPro
                  aria-label='4 of 5 stars'
                  onMouseEnter={() => setHovered(prevState => ({...prevState, star4:true}))}
                  onMouseLeave={() => setHovered(prevState => ({...prevState, star4:false}))}
-                 onClick={(e) => {
-                  e.preventDefault()
-                  setRating(4)
-                 }}>
-                 <svg viewBox="0 0 15 15" stroke={checkRating(4) ? 'gold' : 'gray'} aria-hidden={true} xmlns="http://www.w3.org/2000/svg" className='w-[17px] h-[17px] md:w-[25px] md:h-[25px]'> <path d="M7.22303 0.665992C7.32551 0.419604 7.67454 0.419604 7.77702 0.665992L9.41343 4.60039C9.45663 4.70426 9.55432 4.77523 9.66645 4.78422L13.914 5.12475C14.18 5.14607 14.2878 5.47802 14.0852 5.65162L10.849 8.42374C10.7636 8.49692 10.7263 8.61176 10.7524 8.72118L11.7411 12.866C11.803 13.1256 11.5206 13.3308 11.2929 13.1917L7.6564 10.9705C7.5604 10.9119 7.43965 10.9119 7.34365 10.9705L3.70718 13.1917C3.47945 13.3308 3.19708 13.1256 3.25899 12.866L4.24769 8.72118C4.2738 8.61176 4.23648 8.49692 4.15105 8.42374L0.914889 5.65162C0.712228 5.47802 0.820086 5.14607 1.08608 5.12475L5.3336 4.78422C5.44573 4.77523 5.54342 4.70426 5.58662 4.60039L7.22303 0.665992Z" fill={`${rating !== 0 ? checkRating(4) ? 'gold' : 'none' : star4 || star5 ? 'gray' : 'none'}`} /></svg>
+                 onClick={(e) => selectRating(e,4)}>
+                 <svg viewBox="0 0 15 15" stroke={checkRating(4) ? 'gold' : 'gray'} aria-hidden={true} xmlns="http://www.w3.org/2000/svg" className='w-[17px] h-[17px] md:w-[25px] md:h-[25px]'> <path d="M7.22303 0.665992C7.32551 0.419604 7.67454 0.419604 7.77702 0.665992L9.41343 4.60039C9.45663 4.70426 9.55432 4.77523 9.66645 4.78422L13.914 5.12475C14.18 5.14607 14.2878 5.47802 14.0852 5.65162L10.849 8.42374C10.7636 8.49692 10.7263 8.61176 10.7524 8.72118L11.7411 12.866C11.803 13.1256 11.5206 13.3308 11.2929 13.1917L7.6564 10.9705C7.5604 10.9119 7.43965 10.9119 7.34365 10.9705L3.70718 13.1917C3.47945 13.3308 3.19708 13.1256 3.25899 12.866L4.24769 8.72118C4.2738 8.61176 4.23648 8.49692 4.15105 8.42374L0.914889 5.65162C0.712228 5.47802 0.820086 5.14607 1.08608 5.12475L5.3336 4.78422C5.44573 4.77523 5.54342 4.70426 5.58662 4.60039L7.22303 0.665992Z" fill={`${rating !== 0 ? checkRating(4) ? 'gold' : star4 || star5 ? 'gray' : 'none' : star4 || star5 ? 'gray' : 'none'}`} /></svg>
               </a>
               <a href='#'
                  className={`spr-icon spr-icon-star ${star5 ? 'spr-icon-star-underline' : ''} ${checkRating(5) ? 'spr-icon-star-not-empty' : ''}`}
@@ -115,11 +108,8 @@ export default function ProductReviewsForm ( {product_id}: ProductReviewsFormPro
                  aria-label='5 of 5 stars'
                  onMouseEnter={() => setHovered(prevState => ({...prevState, star5:true}))}
                  onMouseLeave={() => setHovered(prevState => ({...prevState, star5:false}))}
-                 onClick={(e) => {
-                  e.preventDefault()
-                  setRating(5)
-                 }}>
-                 <svg viewBox="0 0 15 15" stroke={checkRating(5) ? 'gold' : 'gray'} aria-hidden={true} xmlns="http://www.w3.org/2000/svg" className='w-[17px] h-[17px] md:w-[25px] md:h-[25px]'> <path d="M7.22303 0.665992C7.32551 0.419604 7.67454 0.419604 7.77702 0.665992L9.41343 4.60039C9.45663 4.70426 9.55432 4.77523 9.66645 4.78422L13.914 5.12475C14.18 5.14607 14.2878 5.47802 14.0852 5.65162L10.849 8.42374C10.7636 8.49692 10.7263 8.61176 10.7524 8.72118L11.7411 12.866C11.803 13.1256 11.5206 13.3308 11.2929 13.1917L7.6564 10.9705C7.5604 10.9119 7.43965 10.9119 7.34365 10.9705L3.70718 13.1917C3.47945 13.3308 3.19708 13.1256 3.25899 12.866L4.24769 8.72118C4.2738 8.61176 4.23648 8.49692 4.15105 8.42374L0.914889 5.65162C0.712228 5.47802 0.820086 5.14607 1.08608 5.12475L5.3336 4.78422C5.44573 4.77523 5.54342 4.70426 5.58662 4.60039L7.22303 0.665992Z" fill={`${rating !== 0 ? checkRating(5) ? 'gold' : 'none' : star5 ? 'gray' : 'none'}`} /></svg>
+                 onClick={(e) => selectRating(e,5)}>
+                 <svg viewBox="0 0 15 15" stroke={checkRating(5) ? 'gold' : 'gray'} aria-hidden={true} xmlns="http://www.w3.org/2000/svg" className='w-[17px] h-[17px] md:w-[25px] md:h-[25px]'> <path d="M7.22303 0.665992C7.32551 0.419604 7.67454 0.419604 7.77702 0.665992L9.41343 4.60039C9.45663 4.70426 9.55432 4.77523 9.66645 4.78422L13.914 5.12475C14.18 5.14607 14.2878 5.47802 14.0852 5.65162L10.849 8.42374C10.7636 8.49692 10.7263 8.61176 10.7524 8.72118L11.7411 12.866C11.803 13.1256 11.5206 13.3308 11.2929 13.1917L7.6564 10.9705C7.5604 10.9119 7.43965 10.9119 7.34365 10.9705L3.70718 13.1917C3.47945 13.3308 3.19708 13.1256 3.25899 12.866L4.24769 8.72118C4.2738 8.61176 4.23648 8.49692 4.15105 8.42374L0.914889 5.65162C0.712228 5.47802 0.820086 5.14607 1.08608 5.12475L5.3336 4.78422C5.44573 4.77523 5.54342 4.70426 5.58662 4.60039L7.22303 0.665992Z" fill={`${rating !== 0 ? checkRating(5) ? 'gold' : star5 ? 'gray' : 'none' : star5 ? 'gray' : 'none'}`} /></svg>
               </a>
             </div>
           </div>
