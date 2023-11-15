4dab2cf7b619c235cfea176e2ea64eeedf1dacf6
JASPER <jasjasper101@gmail.com>
2023-11-14 21:16:59 -0800

    Implement swiper-fade effect to swipers

--- snowtum-shredders/app/products/[...product]/ProductComponents/ProductGallery.tsx
+++ snowtum-shredders/app/products/[...product]/ProductComponents/ProductGallery.tsx
@@ -1,7 +1,8 @@
 'use client'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation } from 'swiper/modules'
+import { Navigation, EffectFade } from 'swiper/modules'
 import 'swiper/css'
+import "swiper/css/effect-fade"
 import { circleXMarkSVG, leftVectorSVG, rightVectorSVG } from '@/app/Misc/Icons'
 import Image from 'next/image'
 
@@ -13,13 +14,17 @@ export default function ProductGallery( {images, name}: ProductGalleryProps ) {
   return (
     <div className='product-gallery  fixed z-20 top-0 left-0 w-full h-full overflow-hidden'>
       <Swiper
-        modules={[Navigation]}
+        modules={[Navigation, EffectFade]}
         navigation={{
           nextEl: '.product-gallery-nav-next',
           prevEl: '.product-gallery-nav-prev',
           disabledClass: 'swiper-button-disabled'
         }}
         loop={true}
+        effect='fade'
+        fadeEffect={{
+          crossFade: true
+        }}
       >
         {images.map((image, index) => {
           return (
