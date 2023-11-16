60abe8572d03093dc7113cc77f713a3f549f4864
JASPER <jasjasper101@gmail.com>
2023-11-16 13:46:27 -0800

    Finished product gallery functionality

--- snowtum-shredders/app/products/[...product]/ProductComponents/ProductGallery.tsx
+++ snowtum-shredders/app/products/[...product]/ProductComponents/ProductGallery.tsx
@@ -5,6 +5,7 @@ import 'swiper/css'
 import "swiper/css/effect-fade"
 import { circleXMarkSVG, leftVectorSVG, rightVectorSVG } from '@/app/Misc/Icons'
 import Image from 'next/image'
+import React, { useEffect, useState } from 'react'
 
 interface ProductGalleryProps {
   images: string[];
@@ -13,43 +14,79 @@ interface ProductGalleryProps {
   setToggleGallery: (prevState: boolean) => void
 }
 export default function ProductGallery( {images, name, toggleGallery, setToggleGallery}: ProductGalleryProps ) {
+  const [transformStyle, setTransformStyle] = useState('translate(0, 0)');
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      const y = (e.clientY / window.innerHeight) * -400; // Multiply by -400 to get negative Y translation
+      const transform = `translate(0, ${y}px)`;
+      setTransformStyle(transform);
+    };
+
+    document.addEventListener('mousemove', handleMouseMove);
+
+    const mediaQuery = window.matchMedia('(min-width: 1024px)');
+
+    const handleResize = () => {
+      if (mediaQuery.matches) {
+        document.addEventListener('mousemove', handleMouseMove);
+      } else {
+        document.removeEventListener('mousemove', handleMouseMove);
+        setTransformStyle('translate(0, 0)')
+      }
+    };
+
+    // Initial setup
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []); // Empty dependency array to run the effect once
+
+
   return (
-    <div className={`product-gallery ${toggleGallery ? '' : 'invisible opacity-0 pointer-events-none'} bg-primary fixed top-0 left-0 w-full h-full overflow-hidden z-50`}>
+    <div className={`product-gallery ${toggleGallery ? '' : 'invisible opacity-0 pointer-events-none'} bg-primary fixed top-0 left-0 w-screen h-screen overflow-hidden z-50`}>
       <Swiper
         modules={[Navigation, EffectFade]}
         navigation={{
           nextEl: '.product-gallery-nav-next',
           prevEl: '.product-gallery-nav-prev',
-          disabledClass: 'swiper-button-disabled'
+          disabledClass: '.swiper-button-disabled'
         }}
         loop={true}
         effect='fade'
         fadeEffect={{
           crossFade: true
         }}
-        className=''
+        className='w-full h-full'
       >
         {images.map((image, index) => {
           return (
-            <SwiperSlide key={index} >
-              <div className='product-gallery-item w-full h-full'>
+            <SwiperSlide key={index} className=''>
+              <div className='product-gallery-item w-full h-full flex justify-center'>
                 <Image
                   src={image}
-                  height={500}
+                  height={200}
                   width={200}
                   alt= {name}
+                  className='mx-auto w-[50%] lg:w-[50%] h-auto lg:h-[150%]'
+                  style={{ transform: transformStyle }}
                 />
               </div>
             </SwiperSlide>
           )
         })}
-        <button className='product-gallery-close' onClick={() => setToggleGallery(!toggleGallery)}>
-          {circleXMarkSVG('black')}
+        <button className='product-gallery-close absolute top-[50px] right-[50px] z-2' onClick={() => setToggleGallery(!toggleGallery)}>
+          {circleXMarkSVG}
         </button>
-        <button className='product-gallery-nav-prev absolute left-[40px]' type='button'>
+        <button className='product-gallery-nav-prev absolute top-[50%] left-[50px] z-2 translate-y-[-50%]' type='button'>
           {leftVectorSVG}
         </button>
-        <button className='product-gallery-nav-next absolute right-[40px]' type='button'>
+        <button className='product-gallery-nav-next absolute top-[50%] right-[50px] z-2 translate-y-[-50%]' type='button'>
           {rightVectorSVG}
         </button>
       </Swiper>
