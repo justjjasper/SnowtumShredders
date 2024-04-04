da25c6cf9ef32849bc3561f5b273f8fde023df58
JASPER <jasjasper101@gmail.com>
2024-04-04 15:04:20 -0700

    adjusted ga4

--- snowtum-shredders/app/layout.tsx
+++ snowtum-shredders/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { GoogleTagManager } from '@next/third-parties/google'
+import { GoogleTagManager, GoogleAnalytics } from '@next/third-parties/google'
 
 // Components
 import NavBar from './components/Header/NavBar'
@@ -32,7 +32,8 @@ export default function RootLayout({
           <Footer/>
         </ReduxProvider>
       </body>
-      <GoogleTagManager gtmId='G-BY2MTYK4JT'/>
+      <GoogleAnalytics gaId='G-BY2MTYK4JT'/>
+      <GoogleTagManager gtmId='GTM-TBKJ9BD9'/>
     </html>
   )
 }
