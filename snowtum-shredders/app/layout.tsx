b939a013d713a1c0ba8f7d52501ddde4c89a92bc
JASPER <jasjasper101@gmail.com>
2024-04-04 12:52:08 -0700

    Adjust GA4 reset

--- snowtum-shredders/app/layout.tsx
+++ snowtum-shredders/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 // Components
 import NavBar from './components/Header/NavBar'
@@ -31,6 +32,7 @@ export default function RootLayout({
           <Footer/>
         </ReduxProvider>
       </body>
+      <GoogleAnalytics gaId='G-BY2MTYK4JT'/>
     </html>
   )
 }
